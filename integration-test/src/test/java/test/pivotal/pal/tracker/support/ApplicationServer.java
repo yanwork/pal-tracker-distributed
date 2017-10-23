4b2d0c629ffa1ee0bd61f36a4ce7c93f94b8a147
Yanwork <yan@yanwork.com>
2017-10-23 17:15:30 -0600

    Zipkin

--- integration-test/src/test/java/test/pivotal/pal/tracker/support/ApplicationServer.java
+++ integration-test/src/test/java/test/pivotal/pal/tracker/support/ApplicationServer.java
@@ -42,6 +42,7 @@ public class ApplicationServer {
             .put("RIBBON_EUREKA_ENABLED", "false")
             .put("APPLICATION_OAUTH_ENABLED", "false")
             .put("REGISTRATION_SERVER_RIBBON_LISTOFSERVERS", "http://localhost:8883")
+            .put("SPRING_ZIPKIN_ENABLED", "false")
             .build()
         );
     }
---
+++ platform-services/zipkin-server/src/main/java/io/pivotal/pal/tracker/zipkin/App.java
@@ -0,0 +1,14 @@
+package io.pivotal.pal.tracker.zipkin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import zipkin.server.EnableZipkinServer;
+
+@SpringBootApplication
+@EnableZipkinServer
+public class App {
+
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
+    }
+}
