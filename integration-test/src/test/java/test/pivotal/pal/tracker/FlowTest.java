9b4b23ccf2273437fba10a1fc027d654fb5108d6
Yanwork <yan@yanwork.com>
2017-10-23 13:51:14 -0600

    Service Discovery

--- integration-test/src/test/java/test/pivotal/pal/tracker/FlowTest.java
+++ integration-test/src/test/java/test/pivotal/pal/tracker/FlowTest.java
@@ -15,7 +15,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Fail.fail;
 import static test.pivotal.pal.tracker.support.MapBuilder.jsonMapBuilder;
 
-@Ignore
+
 public class FlowTest {
 
     private final HttpClient httpClient = new HttpClient();
